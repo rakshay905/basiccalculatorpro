388fe611278e90f573e937e3a94b03de0841c77f
satyam kamboj <satyamkamboj7@gmail.com>
2022-01-10 21:28:25 +0530

    ads

--- app/src/main/java/com/asgroup/basiccalculator/MainActivity.java
+++ app/src/main/java/com/asgroup/basiccalculator/MainActivity.java
@@ -6,15 +6,36 @@ import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this);
+        createInterstitialAd();
+
+        if (interstitialAd.isLoaded()){
+            interstitialAd.show();
+        }
+
         WebView webview = (WebView)findViewById(R.id.webview);
         webview.setWebChromeClient(new WebChromeClient());
         webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+    void createInterstitialAd(){
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad));
+        interstitialAd.loadAd(new AdRequest.Builder().build());
     }
 }
